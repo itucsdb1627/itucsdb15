9e73680a0a17e4d6bad64a4428ffed280c94a4c5
Erdem <Erdem@9.84.123.214>
2016-10-17 17:55:08 +0300

    Site açılış sayfası oluşturuldu. Şablon sayfa belirlendi.

--- server.py
+++ server.py
@@ -13,11 +13,26 @@ def home_page():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
 
+@app.route('/ilgialanlari')
+def ilgialanlari_page():
+    return render_template('ilgialanlari.html')
+@app.route('/kisiler')
+def kisiler_page():
+    return render_template('kisiler.html')
+@app.route('/baglantilar')
+def baglantilar_page():
+    return render_template('baglantilar.html')
+@app.route('/hakkimizda')
+def hakkimizda_page():
+    return render_template('hakkimizda.html')
+@app.route('/isfirsatlari')
+def isfirsatlari_page():
+    return render_template('isfirsatlari.html')
 
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
         port, debug = int(VCAP_APP_PORT), False
     else:
-        port, debug = 5000, True
+        port, debug = 5001, True
     app.run(host='0.0.0.0', port=port, debug=debug)
