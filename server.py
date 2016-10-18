df4bc40edf9d32ebd42f433a3343eda5bdf284fd
Zeynep Ankara <Zeynep Ankara>
2016-10-18 15:46:41 +0300

    hakkimizda.html is changed

--- server.py
+++ server.py
@@ -4,9 +4,7 @@ import os
 from flask import Flask
 from flask import render_template
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def home_page():
@@ -35,4 +33,4 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5001, True
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    app.run(host='0.0.0.0', port=5001, debug=debug)
