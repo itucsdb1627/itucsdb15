4dc79b95d76b33376718dd3d1f73f3d08bbeaf75
buraksimsek <simsekburak1@itu.edu.tr>
2016-11-26 17:23:34 +0200

    Maindata adlı tablo, maindata.py ye alındı. Ayrıca maindata tablosuna İsim ve Soyisim sütunları eklendi.Böylece kullanılacak ana tablo geliştirildi.

---
+++ maindatadb.py
@@ -0,0 +1,90 @@
+import json
+import datetime
+import re
+import os
+import psycopg2 as dbapi2
+
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import request
+from flask.helpers import url_for
+
+
+app = Flask(__name__)
+@app.route('/admin/initdb')
+def initialize_maindata_db():
+    connection=dbapi2.connect(app.config['dsn'])
+    cursor=connection.cursor()
+
+    query=""" DROP TABLE IF EXISTS BAGLANTILAR CASCADE"""
+    cursor.execute(query)
+    query="""CREATE TABLE MAINDATA(ID SERIAL PRIMARY KEY, EMAIL VARCHAR(50) NOT NULL,PASSWORD VARCHAR(50) NOT NULL,NAME VARCHAR(50) NOT NULL,SURNAME VARCHAR(50) NOT NULL,UNIQUE(EMAIL))"""
+    cursor.execute(query)
+    query="""INSERT INTO MAINDATA(EMAIL,PASSWORD,NAME,SURNAME) VALUES('simsekburak','asd','Burak','Simsek')"""
+    cursor.execute(query)
+    connection.commit()
+    return redirect(url_for('admin_page'))
+
+@app.route('/admin',methods=['GET','POST'])
+def admin_page():
+
+        connection = dbapi2.connect(app.config['dsn'])
+        cursor = connection.cursor()
+        if request.method == 'GET':
+          cursor.execute("""SELECT * FROM MAINDATA ORDER BY  EMAIL , PASSWORD""")
+          backupmaindata=cursor.fetchall()
+          connection.commit()
+          maindata = [(key,email,password,name,surname)
+                 for key,email,password,name,surname in cursor]
+        return render_template('admin.html', maindata = backupmaindata)
+@app.route('/admin/deleteuser',methods=['POST','GET'])
+def delete_user():
+        connection = dbapi2.connect(app.config['dsn'])
+        cursor = connection.cursor()
+        if request.method=='POST':
+          eposta=request.form['email']
+          cursor.execute("DELETE FROM MAINDATA WHERE EMAIL= %s",(eposta,))
+
+          cursor.execute("SELECT * FROM MAINDATA")
+          backupmaindata=cursor.fetchall()
+          connection.commit()
+          return redirect(url_for('admin_page',maindata=backupmaindata))
+        elif request.method == 'GET':
+          return redirect(url_for('admin_page',maindata=backupmaindata))
+@app.route('/admin/searchuser',methods=['POST','GET'])
+def search_user():
+
+    if request.method=='POST':
+      emailadd=request.form['emailaddress']
+      connection = dbapi2.connect(app.config['dsn'])
+      cursor = connection.cursor()
+      cursor.execute("SELECT * FROM MAINDATA WHERE EMAIL=%s",(emailadd,))
+      connection.commit()
+      backupmaindata=[(key,email,password,name,surname)
+                      for key,email,password,name,surname in cursor]
+
+      return render_template('updateuser.html',backupmaindata=backupmaindata)
+@app.route('/admin/updateuser/<asdid>',methods=['POST','GET'])
+def update_user(asdid):
+    connection = dbapi2.connect(app.config['dsn'])
+    cursor = connection.cursor()
+    if request.method=='POST':
+
+       posta=request.form['email']
+       psswd=request.form['password']
+       name=request.form['name']
+       surname=request.form['surname']
+       cursor.execute("""UPDATE MAINDATA SET EMAIL=%s,PASSWORD=%s,NAME=%s,SURNAME=%s  WHERE ID= %s""" ,(posta,psswd,name,surname,asdid))
+       connection.commit()
+
+       return redirect(url_for('admin_page'))
+    elif request.method=='GET':
+        return render_template('updateuser.html')
+@app.route('/admin/delete')
+def  delete_db():
+    connection = dbapi2.connect(app.config['dsn'])
+    cursor = connection.cursor()
+    cursor.execute("""DROP TABLE MAINDATA""" )
+    connection.commit()
+    return render_template('admin.html')
