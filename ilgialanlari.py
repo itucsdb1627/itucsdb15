a3c9462ad8fdaa48f7ed74cbfcf712cec23072dd
hilalgulsen <gulsenhi@itu.edu.tr>
2016-11-29 01:38:05 +0200

    Publishes tablosu oluşturuldu.Maindata tablosunda personid referans edildi.Ekleme,silme,arama,güncelleme işlemleri yapıldı.

--- ilgialanlari.py
+++ ilgialanlari.py
@@ -7,33 +7,15 @@ import psycopg2 as dbapi2
 from flask import render_template
 from config import app
 
+from grup import *
+from publishes import *
+
 class Grup:
     def __init__(self, GroupName, Explanation,StartDate, Sector):
         self.GroupName = GroupName
         self.Explanation = Explanation
         self.StartDate = StartDate
         self.Sector = Sector
-
-@app.route('/ilgialanlari/init')
-def init_groups_database():
-    connection = dbapi2.connect(app.config['dsn'])
-    cursor = connection.cursor()
-    cursor.execute('''
-    DROP TABLE IF EXISTS GRUP CASCADE;
-    ''')
-    query = """CREATE TABLE IF NOT EXISTS GRUP (
-    ID SERIAL PRIMARY KEY,
-    GROUPNAME VARCHAR(90) NOT NULL,
-    EXPLANATION VARCHAR(300) NOT NULL,
-    STARTDATE DATE NOT NULL,
-    SECTOR VARCHAR(90) NOT NULL,
-    PERSONID INTEGER,
-    FOREIGN KEY (PERSONID)
-    REFERENCES MAINDATA (ID)
-    ON DELETE CASCADE)"""
-    cursor.execute(query)
-    connection.commit()
-    return redirect(url_for('home_page'))
 
 
 def add_groups(self, grup):
@@ -49,73 +31,34 @@ def add_groups(self, grup):
 @app.route('/ilgialanlari/<personid>', methods=['GET', 'POST'])
 def ilgialanlari_page(personid):
     if request.method == 'GET':
-        connection = dbapi2.connect(app.config['dsn'])
-        cursor = connection.cursor()
-        cursor.execute("""SELECT * FROM GRUP WHERE PERSONID = %s """,[personid])
-        connection.commit()
-        grup = [(key, GroupName,Explanation,StartDate,Sector,personid)
-                        for key, GroupName, Explanation,StartDate ,Sector, personid in cursor]
-        return render_template('ilgialanlari.html', grup = grup,personid=personid)
-
+        grup=expressgrup_page(personid)
+        publishes=expresspublishes_page(personid)
+        return render_template('ilgialanlari.html',grup=grup,publishes=publishes,personid=personid)
 
     else:
-        if 'Add' in request.form:
-            GroupName = request.form['GroupName']
-            Explanation = request.form['Explanation']
-            StartDate = request.form['StartDate']
-            Sector = request.form['Sector']
-            connection = dbapi2.connect(app.config['dsn'])
-            cursor = connection.cursor()
-            cursor.execute("""
-            INSERT INTO GRUP (GROUPNAME, EXPLANATION,STARTDATE,SECTOR,PERSONID)
-            VALUES (%s, %s, %s, %s, %s) """,
-            (GroupName, Explanation,StartDate,Sector,personid))
-            connection.commit()
+        if 'Addpublishes' in request.form:
+            addpublishes_page(personid)
             return redirect(url_for('ilgialanlari_page',personid=personid))
-
+        elif 'Add' in request.form:
+            addgrup_page(personid)
+            return redirect(url_for('ilgialanlari_page',personid=personid))
+        elif 'Deletepublishes' in request.form:
+            deletepublishes_page(personid)
+            return redirect(url_for('ilgialanlari_page',personid=personid))
         elif 'Delete' in request.form:
-            id = request.form['id']
-            connection = dbapi2.connect(app.config['dsn'])
-            cursor = connection.cursor()
-            cursor.execute( """ DELETE FROM GRUP WHERE ID =%s """,[id])
-            connection.commit()
+            deletegrup_page(personid)
             return redirect(url_for('ilgialanlari_page',personid=personid))
+        elif 'Updatepublishes' in request.form:
+            publishesid=updatepublishes_page(personid)
+            return render_template('updatepublishes.html', key = publishesid,personid=personid)
         elif 'Update' in request.form:
-            grupid = request.form['id']
+            grupid=updategrup_page(personid)
             return render_template('updategroups.html', key = grupid,personid=personid)
+        elif 'Searchpublishes' in request.form:
+            publishes=searchpublishes_page(personid)
+            return render_template('ilgialanlari.html',publishes=publishes,personid=personid)
         elif 'Search' in request.form:
-            GroupName = request.form['GroupName']
-            connection = dbapi2.connect(app.config['dsn'])
-            cursor = connection.cursor()
-            cursor.execute( "SELECT * FROM GRUP WHERE GROUPNAME LIKE %s",(GroupName,))
-            connection.commit()
-            grup = [(key, GroupName,Explanation, StartDate, Sector,personid)
-                        for key, GroupName, Explanation, StartDate, Sector,personid in cursor]
+            grup=searchgrup_page(personid)
             return render_template('ilgialanlari.html',grup = grup,personid=personid)
 
-
-@app.route('/ilgialanlari/updategroups/<grupid>,<personid>', methods=['GET', 'POST'])
-def update_groups(grupid,personid):
-    if request.method == 'GET':
-        return render_template('updategroups.html')
-    else:
-         if 'Update' in request.form:
-             GroupName = request.form['GroupName']
-             Explanation = request.form['Explanation']
-             StartDate = request.form['StartDate']
-             Sector = request.form['Sector']
-             connection = dbapi2.connect(app.config['dsn'])
-             cursor = connection.cursor()
-             cursor.execute(""" UPDATE GRUP SET GROUPNAME = %s, EXPLANATION= %s, STARTDATE= %s, SECTOR= %s WHERE ID = %s """,
-             (GroupName, Explanation, StartDate, Sector, grupid))
-             connection.commit()
-             return redirect(url_for('ilgialanlari_page',personid=personid))
-
-@app.route('/ilgialanlari/deletegroups')
-def  delete_groups():
-    connection = dbapi2.connect(app.config['dsn'])
-    cursor = connection.cursor()
-    cursor.execute("""DROP TABLE GRUP""" )
-    connection.commit()
-    return render_template('ilgialanlari.html')
 
